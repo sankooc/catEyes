eb37d062a328aaec47e40a7f807fc134ce90f022
sankooc <sankooc@yahoo.com.cn>
2013-05-03 16:00:29 +0800

    init commit

---
+++ core/src/main/java/org/cateyes/core/Resolver.java
@@ -0,0 +1,5 @@
+package org.cateyes.core;
+
+public interface Resolver {
+
+}
---
+++ core/src/main/java/org/cateyes/core/youku/YoukuResolver.java
@@ -0,0 +1,15 @@
+package org.cateyes.core.youku;
+
+import java.net.URL;
+
+import org.cateyes.core.Resolver;
+
+public class YoukuResolver implements Resolver {
+	public void resolv(URL url) {
+
+	}
+
+	public void resolvSid(String sid) {
+
+	}
+}
